2c7ccfa029d9c5ce7d819c9e2b4b1c9d97d16575
Filip Szczepanski <dev@szczepanski.us>
2017-07-31 22:14:33 +0200

    Moved App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import store from '../store'
+import store from '../store';
 import { getShopListData } from '../actions/shopListItems';
-import Layout from '../components/Layout'
-import './App.css';
+import Layout from './Layout';
 
 const SHOP_LIST_DATA_SRC = 'data/example.json';
 
